cff877c38ea2bd500e39e8fbde847e8d84543acd
MaxJara <maxmata77@gmail.com>
2021-07-05 21:35:02 -0500

    prueba

--- JavaSEBasicoAvanzado/JavaSEBasicoAvanzado-32.AbstractClases/AmazonViewer/src/com/anncode/amazonviewer/Main.java
+++ JavaSEBasicoAvanzado/JavaSEBasicoAvanzado-32.AbstractClases/AmazonViewer/src/com/anncode/amazonviewer/Main.java
@@ -7,17 +7,32 @@ import java.util.Scanner;
 
 import com.anncode.amazonviewer.model.Book;
 import com.anncode.amazonviewer.model.Chapter;
+import com.anncode.amazonviewer.model.Film;
 import com.anncode.amazonviewer.model.Magazine;
 import com.anncode.amazonviewer.model.Movie;
 import com.anncode.amazonviewer.model.Serie;
 import com.anncode.makereport.Report;
 import com.anncode.util.AmazonUtil;
 
+
+
+/**
+ * <h1>AmazonViewer</h1>
+ * AmazonViewer es un programa que permite visualizar Movies, Series con sus respectivos Chapters,
+ * Books y Magazines. Te permite generar reportes generales y con fecha del día.
+ * <p>
+ * Existen algunas reglas como que todos los elementos pueden ser visualizadors o leídos a excepción
+ * de las Magazines, estás solo pueden ser vistas a modo de exposición sin ser leídas.
+ * 
+ * @author maxjara
+ * @version 1.1
+ * @since 2021
+ * 
+ * */
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
 		showMenu();
 
 	}
@@ -28,7 +43,7 @@ public class Main {
 			
 			System.out.println("BIENVENIDOS AMAZON VIEWER");
 			System.out.println("");
-			System.out.println("Selecciona el nÃºmero de la opciÃ³n deseada");
+			System.out.println("Selecciona el número de la opción deseada");
 			System.out.println("1. Movies");
 			System.out.println("2. Series");
 			System.out.println("3. Books");
@@ -69,7 +84,7 @@ public class Main {
 	
 				default:
 					System.out.println();
-					System.out.println("....Â¡Â¡Selecciona una opciÃ³n!!....");
+					System.out.println("....¡¡Selecciona una opción!!....");
 					System.out.println();
 					exit = 1;
 					break;
@@ -105,18 +120,7 @@ public class Main {
 			}
 			if (response > 0) {
 				Movie movieSelected = movies.get(response-1);
-				movieSelected.setViewed(true);
-				Date dateI = movieSelected.startToSee(new Date());
-				
-				for (int i = 0; i < 100000; i++) {
-					System.out.println("..........");
-				}
-				
-				//Termine de verla
-				movieSelected.stopToSee(dateI, new Date());
-				System.out.println();
-				System.out.println("Viste: " + movieSelected);
-				System.out.println("Por: " + movieSelected.getTimeViewed() + " milisegundos");
+				movieSelected.view();
 			}
 			
 			
@@ -181,18 +185,7 @@ public class Main {
 			
 			if(response > 0) {
 				Chapter chapterSelected = chaptersOfSerieSelected.get(response-1);
-				chapterSelected.setViewed(true);
-				Date dateI = chapterSelected.startToSee(new Date());
-				
-				for (int i = 0; i < 100000; i++) {
-					System.out.println("..........");
-				}
-				
-				//Termine de verla
-				chapterSelected.stopToSee(dateI, new Date());
-				System.out.println();
-				System.out.println("Viste: " + chapterSelected);
-				System.out.println("Por: " + chapterSelected.getTimeViewed() + " milisegundos");
+				chapterSelected.view();
 			}
 		}while(exit !=0);
 	}
@@ -207,7 +200,7 @@ public class Main {
 			System.out.println();
 			
 			for (int i = 0; i < books.size(); i++) { //1. Book 1
-				System.out.println(i+1 + ". " + books.get(i).getTitle() + " LeÃ­do: " + books.get(i).isReaded());
+				System.out.println(i+1 + ". " + books.get(i).getTitle() + " Leído: " + books.get(i).isReaded());
 			}
 			
 			System.out.println("0. Regresar al Menu");
@@ -223,18 +216,7 @@ public class Main {
 			
 			if(response > 0) {
 				Book bookSelected = books.get(response-1);
-				bookSelected.setReaded(true);
-				Date dateI = bookSelected.startToSee(new Date());
-				
-				for (int i = 0; i < 100000; i++) {
-					System.out.println("..........");
-				}
-				
-				//Termine de verla
-				bookSelected.stopToSee(dateI, new Date());
-				System.out.println();
-				System.out.println("LeÃ­ste: " + bookSelected);
-				System.out.println("Por: " + bookSelected.getTimeReaded() + " milisegundos");
+				bookSelected.view();
 			}
 			
 		}while(exit !=0);
--- JavaSEBasicoAvanzado/JavaSEBasicoAvanzado-32.AbstractClases/AmazonViewer/src/com/anncode/amazonviewer/model/Film.java
+++ JavaSEBasicoAvanzado/JavaSEBasicoAvanzado-32.AbstractClases/AmazonViewer/src/com/anncode/amazonviewer/model/Film.java
@@ -1,5 +1,19 @@
 package com.anncode.amazonviewer.model;
 
+
+/**
+ * <h1>Film</h1>
+ * Film es una clase padre abstracta
+ * <p>
+ * Esta clase es la clase base de la familia Films, como es abstracta 
+ * no pueden crearse instancias. Continene el método abstracto 
+ * {@code view()} que es obligatorio implementar para todo aquél que pertenezca a la familia.
+ * 
+ * 
+ * @author maxjara
+ * @version 1.1
+ * @since 2021
+ * */
 public abstract class Film {
 	
 	private String title;
@@ -52,7 +66,7 @@ public abstract class Film {
 	public String isViewed() {
 		String visto = "";
 		if(viewed == true) {
-			visto = "SÃ­";
+			visto = "Sí";
 		}else {
 			visto = "No";
 		}
@@ -67,7 +81,9 @@ public abstract class Film {
 		this.viewed = viewed;
 	}
 	
-	
+	/**
+	 * {@code view()} es un método abstracto obligatorio de implementar
+	 * */
 	public abstract void view();
 
 }

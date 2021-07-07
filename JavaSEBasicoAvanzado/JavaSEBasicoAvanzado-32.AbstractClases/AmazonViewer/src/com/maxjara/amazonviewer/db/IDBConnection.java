42204f6b803291f9aeab9abe938ac0a0c22a05f8
MaxJara <maxmata77@gmail.com>
2021-07-06 20:33:27 -0500

    Se genero conexion a la base de datos

---
+++ JavaSEBasicoAvanzado/JavaSEBasicoAvanzado-32.AbstractClases/AmazonViewer/src/com/maxjara/amazonviewer/db/IDBConnection.java
@@ -0,0 +1,23 @@
+package com.maxjara.amazonviewer.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import static com.maxjara.amazonviewer.db.DataBase.*;
+
+public interface IDBConnection {
+	
+	default Connection connectToDB() {
+		Connection connection = null;
+		
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			connection = DriverManager.getConnection(URL+DB, USER, PASSWORD);
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}finally {
+			return connection;
+		}
+	}
+
+}
